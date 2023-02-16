d364c56a39f3823b411a22f0289be5fbd625c33a
Robert Amoah <mr_robertamoah@yahoo.com>
2023-02-16 22:53:12 +0000

    done with 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: this prints specific text using
+ * a different function from printf and puts
+ *
+ * Return: returns 0 (Success)
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and the piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
