b00d70f82bec6c4ad49c9eaf95ff83d20d6cfdfd
Robert Amoah <mr_robertamoah@yahoo.com>
2023-02-16 21:44:32 +0000

    done with 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,19 @@
-#!/bin/bash
+#include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: this function prints the sizes of various types
+ *
+ * Return: returns 0 (success)
+ */
+
+int main(void)
+{
+	printf("Size of a char: %d bytes(s)\n", (int) sizeof(char));
+	printf("Size of a int: %d bytes(s)\n", (int) sizeof(int));
+	printf("Size of a long int: %d bytes(s)\n", (int) sizeof(long int));
+	printf("Size of a long long int: %d bytes(s)\n", (int) sizeof(long long int));
+	printf("Size of a float: %d bytes(s)\n", (int) sizeof(float));
+	return (0);
+}
