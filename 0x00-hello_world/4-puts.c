abea98d7bea871a4bc06a69c9ef1344a474bc1f6
Robert Amoah <mr_robertamoah@yahoo.com>
2023-02-16 10:32:12 +0000

    corrected 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,15 @@
-#!/bin/bash
+#include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: 'this is to print text'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
