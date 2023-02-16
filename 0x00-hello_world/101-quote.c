b5ea2325e267bb5920b33090803d2f848d9a5431
Robert Amoah <mr_robertamoah@yahoo.com>
2023-02-16 23:13:18 +0000

    corrected 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,8 +11,8 @@
 
 int main(void)
 {
-	char* data[] =  "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	fprintf(stderr, data);
+	char data[] =  "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	fprintf(stderr, "%s", data);
 
 	return (1);
 }
