da06deedc46de951dad1c71b09044ec97cbd7356
Robert Amoah <mr_robertamoah@yahoo.com>
2023-02-16 21:00:15 +0000

    done with 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,15 @@
-#!/bin/bash
+#include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: this prints specific text using printf
+ *
+ * Return: Always returns 0 (Success)
+ */
+
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
