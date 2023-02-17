2b0f87e0547d3d18dce230150a7f83fea4dfb56c
Robert Amoah <mr_robertamoah@yahoo.com>
2023-02-17 23:07:51 +0000

    done with 7-print_tebahpal.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print alphabets in lowercase using the putchar
+ *
+ * Return: returns 0 (Success)
+ */
+
+int main(void)
+{
+	char letter = 'z';
+
+	while (letter >= 'a')
+	{
+		putchar(letter);
+		letter--;
+	}
+
+	putchar('\n');
+	return (0);
+}
