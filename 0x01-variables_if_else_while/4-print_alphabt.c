f08d36307eda0134a375f61cf55d87b405628ab3
Robert Amoah <mr_robertamoah@yahoo.com>
2023-02-17 22:05:46 +0000

    done with 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print alphabets in lowercase using the putchar
+ *
+ * Return: returns 0 (Success)
+ */
+
+int main(void)
+{
+	char letters[] = "abcdefghijklmnopqrstuvwxyz";
+	int counter;
+
+	for (counter = 0; counter < 26; counter++)
+	{
+		if (letters[counter] == 'e' || letters[counter] == 'q')
+			continue;
+		putchar(letters[counter]);
+	}
+
+	for (counter = 0; counter < 26; counter++)
+	{
+		if (letters[counter] == 'e' || letters[counter] == 'q')
+			continue;
+		putchar((char) toupper(letters[counter]));
+	}
+
+	putchar('\n');
+	return (0);
+}
