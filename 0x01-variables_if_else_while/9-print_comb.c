b8aecda328497275b7e67125f7adc45eabe80d46
Robert Amoah <mr_robertamoah@yahoo.com>
2023-02-18 00:18:34 +0000

    done with 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print alphabets in lowercase using the putchar
+ *
+ * Return: returns 0 (Success)
+ */
+
+int main(void)
+{
+	int max_num = 99;
+	int counter = 0;
+
+	while (counter < max_num)
+	{
+		/**
+		 * if (counter != 0 && (counter % 11 == 0))
+		 *	continue;
+		 */
+		putchar('0' + counter);
+
+		if (counter != max_num - 1)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		else
+		{
+			putchar('\n');
+		}
+
+		counter++;
+	}
+	return (0);
+}
