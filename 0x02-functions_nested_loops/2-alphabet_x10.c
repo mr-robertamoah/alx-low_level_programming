d9aa23993cbd9510de62ab218822dc9ee38d14c2
Robert Amoah <mr_robertamoah@yahoo.com>
2023-02-21 14:11:57 +0000

    done with 2-alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-alphabet_x10.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ *
+ * Description: print alphabets in lowercase using the _putchar, 10 times
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char alphabets[] = "abcdefghijklmnopqrstuvwxyz";
+	int len = 26;
+	int counter = 0;
+	int times = 0;
+
+	while(times < 10)
+	{
+
+		while (counter < len)
+		{
+			_putchar(alphabets[counter]);
+			counter++;
+		}
+
+		_putchar('\n');
+		times++;
+	}
+}
