e552d66490e1cdd5f11132404d95a2d52e7eb74f
Robert Amoah <mr_robertamoah@yahoo.com>
2023-02-22 05:46:12 +0000

    done with 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * n_is_multiple_m - modulus function
+ *
+ * Description: determines if n is a multiple of m
+ *
+ * Return: returns 0 (Success)
+ */
+int n_is_multiple_m(int n, int m)
+{
+	if (m < n && (n % m) == 0)
+		return (1);
+
+	return (0);
+}
+
+/**
+ * main - Entry point
+ *
+ * Description: prints all multiples of 3 or 5
+ *
+ * Return: returns 0 (Success)
+ */
+
+int main(void)
+{
+	int sum = 0;
+	int count = 0;
+
+	while (count < 1024)
+	{
+		if(n_is_multiple_m(count, 3) || n_is_multiple_m(count, 5))
+			sum = sum + count;
+	}
+
+	return (0);
+}
