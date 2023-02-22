c1e1a0710b8bb8cfddc8fc2efbf9ec50eba42f2a
Robert Amoah <mr_robertamoah@yahoo.com>
2023-02-22 05:55:48 +0000

    corrected 101

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,7 +1,45 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
+ * print_n - print number
+ * @n: the int to print from
+ *
+ * Description: print one or two digits numbers
+ *
+ * Return: void
+ */
+void print_n(int n)
+{
+	if (n > 9)
+		_putchar('0' + n);
+	_putchar('0' + n);
+}
+/**
+ * print_number - print number
+ * @n: the int to print from
+ *
+ * Description: print one or two digits numbers
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	int div = n / 10;
+	int mod = n % 10;
+
+	if (div >= 1)
+	{
+		print_number(div);
+		print_number(mod);
+		return;
+	}
+
+	print_n(n);
+}
+/**
  * n_is_multiple_m - modulus function
+ * @n: the input which is being determined to be a multiple
+ * @m: the input which serves as multiple
  *
  * Description: determines if n is a multiple of m
  *
@@ -30,9 +68,10 @@ int main(void)
 
 	while (count < 1024)
 	{
-		if(n_is_multiple_m(count, 3) || n_is_multiple_m(count, 5))
+		if (n_is_multiple_m(count, 3) || n_is_multiple_m(count, 5))
 			sum = sum + count;
 	}
 
+	print_number(sum);
 	return (0);
 }
