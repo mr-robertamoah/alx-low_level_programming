fb324ee0649c14a7bb772eb6036bf960b3fc2e06
Robert Amoah <mr_robertamoah@yahoo.com>
2023-02-23 08:01:49 +0000

    done with 0-main.c

---
+++ 0x02-functions_nested_loops/0x03-debugging/0-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: prints output of positive_or_negative to
+ * test if it prints the right things
+ *
+ * Return: returns 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/0x03-debugging/README.md
@@ -0,0 +1,3 @@
+# DEBUGGINH
+
+This directory contains files that help to check for bugs in C source codes.
