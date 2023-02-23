3f27e1d73b3ae2b1afb6099adb7d089e1d98329d
Robert Amoah <mr_robertamoah@yahoo.com>
2023-02-23 23:23:15 +0000

    done with 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - uppercase check
+ *
+ * Description: prints the nine digits
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+		_putchar(c);
+
+	_putchar('\n');
+}
