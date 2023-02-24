f2fdc9039788c87b946042773026ffbf9e02b4fb
Robert Amoah <mr_robertamoah@yahoo.com>
2023-02-24 05:54:08 +0000

    correction to 100

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,19 +10,19 @@
  */
 void print_number(int n)
 {
+	unsigned int x;
 	if (n < 0)
 	{
 		_putchar('-');
-		n = n * -1;
+		x = n * -1;
 	}
-	else if (n == 0)
+	else
 	{
-		_putchar('0');
-		return;
+		x = n;
 	}
 
-	if (n / 10)
-		print_number(n / 10);
+	if (x / 10)
+		print_number(x / 10);
 
-	_putchar('0' + n % 10);
+	_putchar('0' + (x % 10));
 }
