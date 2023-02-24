8ca65c67dd038cbeea3cf812d20838b684e7cf59
Robert Amoah <mr_robertamoah@yahoo.com>
2023-02-24 05:35:21 +0000

    corrected 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,24 +10,19 @@
  */
 void print_number(int n)
 {
-	int div = n / 10;
-	int mod = n % 10;
-
 	if (n < 0)
 	{
 		_putchar('-');
 		n = n * -1;
 	}
-
-	if (div > 9)
+	else if (n == 0)
 	{
-		print_number(div);
-		print_number(mod);
+		_putchar('0');
 		return;
 	}
 
-	if (n > 9)
-		_putchar('0' + n / 10);
+	if (n / 10)
+		print_number(n / 10);
 
 	_putchar('0' + n % 10);
 }
--- 0x04-more_functions_nested_loops/test.c
+++ 0x04-more_functions_nested_loops/test.c
@@ -18,5 +18,7 @@ print_number(98);
     _putchar('\n');
     print_number(-98);
     _putchar('\n');
+    print_number(-900200);
+    _putchar('\n');
 	return (0);
 }
