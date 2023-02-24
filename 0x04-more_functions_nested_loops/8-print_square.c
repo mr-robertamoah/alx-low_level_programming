fbb89621d8863ff88e4ad7d4edeca344f26b4b89
Robert Amoah <mr_robertamoah@yahoo.com>
2023-02-24 00:41:30 +0000

    done with 8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_square - print square
+ * @n: the size of the square
+ *
+ * Description: printing a square with #
+ *
+ * Return: void
+ */
+
+void print_square(int n)
+{
+	int c = 1, b;
+
+	while (c <= n)
+	{
+		b = 1;
+
+		while (b <= n)
+		{
+			_putchar('#');
+
+			b++;
+		}
+
+		_putchar('\n');
+		c++;
+	}
+}
