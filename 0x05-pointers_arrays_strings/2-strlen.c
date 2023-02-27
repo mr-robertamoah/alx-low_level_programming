c58caa5a276dba0141aedfcb1beec349281975ce
Robert Amoah <mr_robertamoah@yahoo.com>
2023-02-27 10:31:59 +0000

    done with 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * _strlen - length of string
+ * @s: the pointer of the string
+ *
+ * Description: gets the length of a string
+ *
+ * Return: int
+ */
+
+int _strlen(char *s)
+{
+	char i = 0;
+	int count;
+
+	while (*(s + i))
+	{
+		count++;
+
+		i++;
+	}
+
+	return (count);
+}
