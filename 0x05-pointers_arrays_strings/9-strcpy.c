df200933d913569e5f4e61ab21d5160d8a0ad656
Robert Amoah <mr_robertamoah@yahoo.com>
2023-02-28 02:36:53 +0000

    done with 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * _strlen - length of string
+ * @s: the pointer of the string
+ *
+ * Description: gets the length of a string
+ *
+ * Return: int
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	return (i);
+}
+
+/**
+ * _strcpy - copies string
+ * @src: the pointer of the string
+ * @dest: the pointer of the destination buffer
+ *
+ * Description: copies one string into a buffer location
+ *
+ * Return: char *
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int len = _strlen(src) + 1;
+	int i = 0;
+
+	while (i <= len)
+	{
+		dest[i] = src[i];
+
+		i++;
+	}
+
+	return (dest);
+}
--- 0x05-pointers_arrays_strings/test.c
+++ 0x05-pointers_arrays_strings/test.c
@@ -8,20 +8,11 @@
  */
 int main(void)
 {
-int array[5];
+char s1[98];
+    char *ptr;
 
-    array[0] = 98;
-    array[1] = 402;
-    array[2] = -198;
-    array[3] = 298;
-    array[4] = -1024;
-    print_array(array, 5);
-
-int array1[1] = {
-		98
-	};
-
-	print_array(array1, 0);
-	print_array(array1, -1);
+    ptr = _strcpy(s1, "First, solve the problem. Then, write the code\n");
+    printf("%s", s1);
+    printf("%s", ptr);
     return (0);
 }
