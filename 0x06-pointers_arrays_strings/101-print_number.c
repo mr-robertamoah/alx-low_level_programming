7bc5ceaea6f854492dd398dc4f728a121193fb44
Robert Amoah <mr_robertamoah@yahoo.com>
2023-03-02 11:14:10 +0000

    done with 101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_number - print
+ * @n: integer to print
+ *
+ * Description: print an integer using _putchar
+ *
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	if (n < 0 )
+	{
+		_putchar('-');
+		n *= -1;
+	}
+
+	if (n / 10)
+		print_number(n / 10);
+
+	_putchar('0' + (n % 10));
+}
