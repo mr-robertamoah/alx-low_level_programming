d0c2ee3de6306f87f8d2cbd5634bbb3809b5907c
Robert Amoah <mr_robertamoah@yahoo.com>
2023-03-05 16:09:18 +0000

    done with 100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * set_string - set
+ * @s: pointer to pointer
+ * @to: pointer to string
+ *
+ * Description: sum the diagonals of the matrix
+ * chars c in s
+ *
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
