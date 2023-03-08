f62438f67b014583603d7d63d76f1c0625bc0f67
Robert Amoah <mr_robertamoah@yahoo.com>
2023-03-08 21:02:10 +0000

    done with 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,26 @@
 #include "main.h"
 
 /**
+ * is_sqr - multiplication
+ * @a: integer
+ * @b: integer
+ *
+ * Description: determines if square of b is a
+ *
+ * Return: returns int
+ */
+
+int is_sqr(int a, int b)
+{
+	if (b * b == a)
+		return (b);
+	else if (b == 0 && a != b)
+		return (-1);
+
+	return (is_sqr(a, b - 1));
+}
+
+/**
  * _sqrt_recursion - division
  * @n: integer
  *
@@ -11,10 +31,10 @@
 
 int _sqrt_recursion(int n)
 {
-	if (n == 0)
-		return (1);
-	else if (n < 0)
+	if (n < 0)
 		return (-1);
+	else if (n < 2)
+		return (n);
 
-	return (n * _sqrt_recursion(n - 1));
+	return (is_sqr(n, n - 1));
 }
