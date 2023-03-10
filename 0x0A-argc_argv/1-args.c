2be82729e2e0d089aa863ecd4977265fd3cca1d9
Robert Amoah <mr_robertamoah@yahoo.com>
2023-03-10 15:24:55 +0000

    done with 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: count of arguments
+ * @argv: array of string arguments
+ *
+ * Description: print alphabets in lowercase using the putchar
+ *
+ * Return: returns 0 (Success)
+ */
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
