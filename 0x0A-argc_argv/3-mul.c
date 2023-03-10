497e58da6d75b0039a523829059fcfda87bb7a83
Robert Amoah <mr_robertamoah@yahoo.com>
2023-03-10 16:03:53 +0000

    done with 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: count of arguments
+ * @argv: array of string arguments
+ *
+ * Description: print alphabets in lowercase using the putchar
+ *
+ * Return: returns 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc == 3)
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	else
+		puts("Error");
+
+	return (0);
+}
