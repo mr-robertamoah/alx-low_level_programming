521883a97002433f062b5b58e972a5771e844b48
Robert Amoah <mr_robertamoah@yahoo.com>
2023-03-14 08:11:00 +0000

    done with 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strdup - create array
+ * @str: string to make a copy of
+ *
+ * Description: create a copy of a string in
+ * memory
+ *
+ * Return: pointer to char
+ */
+
+char *_strdup(char *str)
+{
+	char *p;
+	unsigned int i;
+	unsigned int len = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len] != '\0')
+		len++;
+
+	p = malloc(len * sizeof(char));
+
+	if (!p)
+		return (NULL);
+
+	for (i = 0; i < len + 1; i++)
+		*(p + i) = *(str + i);
+
+	return (p);
+}
