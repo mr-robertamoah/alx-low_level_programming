237aacf78665f3f2e24bd88511a7ffdeda284606
Robert Amoah <mr_robertamoah@yahoo.com>
2023-03-16 19:52:28 +0000

    done with 1-stringng_nconcat.c

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,63 @@
+#include "main.h"
+
+/**
+ * _strlen - length
+ * @s: pointer to string
+ *
+ * Description: find length of string
+ *
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	return (i);
+}
+
+/**
+ * string_nconcat - concatenate
+ * @s1: pointer to string
+ * @s2: pointer to string
+ * @n: integer
+ *
+ * Description: concatenate s1 with n chars of s2
+ *
+ * Return: char *
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *p;
+	unsigned int i, j, len1, len2;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	p = malloc((len1 + (n >= len2 ? len2 : n) + 1));
+
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+		*(p + i) = s1[i];
+
+	for (j = 0; j < len2; j++)
+	{
+		*(p + i + j) = s2[j];
+		if (j + 1 == n || s2[j + 1] == '\0')
+		{
+			*(p + i + j + 1) = '\0';
+			break;
+		}
+	}
+
+	return (p);
+}
