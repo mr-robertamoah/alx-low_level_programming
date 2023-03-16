64e538e815d7148af6d610674ee05c793d67c7ef
Robert Amoah <mr_robertamoah@yahoo.com>
2023-03-16 20:54:27 +0000

    corrected 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,9 +15,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *p;
 	unsigned int i = 0;
 
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
 	p = malloc(nmemb * size);
 
-	if (p == NULL || nmemb == 0 || size == 0)
+	if (p == NULL)
 		return (NULL);
 
 	while (i < nmemb * size)
