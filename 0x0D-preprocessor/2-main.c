68a81e9e0d8e10684d8ad448e43c8b3165a1ffcd
Robert Amoah <mr_robertamoah@yahoo.com>
2023-03-20 20:37:48 +0000

    done with 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print the file name
+ *
+ * Return: returns 0 (Success)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
