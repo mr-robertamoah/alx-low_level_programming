a20632cf2385aadc4be33d2e587fb066a2e01adc
Robert Amoah <mr_robertamoah@yahoo.com>
2023-03-30 23:14:27 +0100

    done with 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * print_before_main - print
+ *
+ * Description: print a sentence before the main function
+ *
+ * Return: void
+ */
+
+void print_before_main(void)
+{
+	printf("
+		You're beat! and yet, you must allow,\nI bore my house upon my back!\n"
+	);
+}
