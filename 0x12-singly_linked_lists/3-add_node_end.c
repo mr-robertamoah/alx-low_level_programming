fbccca08c871caa6624d4ac22d69c671ecca021a
Robert Amoah <mr_robertamoah@yahoo.com>
2023-03-30 11:57:22 +0100

    done with 3-add_node_end.c

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,62 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+ * create_node - create node
+ * @tmp: pointer to struct list_s
+ * @str: string
+ *
+ * Description: create node to the beginning of list
+ *
+ * Return: list_t
+ */
+list_t *create_node(list_t *tmp, const char *str)
+{
+	char *s;
+	int len = 0;
+
+	if (str != NULL)
+	{
+		s = strdup(str);
+		while (s[len] != '\0')
+			len++;
+	}
+
+	tmp->str = s;
+	tmp->len = len;
+
+	return (tmp);
+}
+
+/**
+ * add_node_end - add node
+ * @head: pointer to struct list_s pointer
+ * @str: string
+ *
+ * Description: add node to the end of list
+ *
+ * Return: list_t
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *tmp, *nextnode;
+
+	tmp = malloc(sizeof(list_t));
+
+	if (tmp == NULL)
+		return (NULL);
+	tmp = create_node(tmp, str);
+
+	if (*head == NULL)
+	{
+		*head = tmp;
+		return (tmp);
+	}
+	nextnode = *head;
+	while (nextnode->next != NULL)
+		nextnode = nextnode->next;
+
+	nextnode->next = tmp;
+
+	return (tmp);
+}
