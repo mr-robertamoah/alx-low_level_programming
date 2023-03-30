a452fdbe090401d77b5829c1aaa2133e14edf0d9
Robert Amoah <mr_robertamoah@yahoo.com>
2023-03-30 11:35:16 +0100

    done with 2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,55 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+ * create_node - create node
+ * @tmp: pointer to struct list_s
+ * @str: string
+ *
+ * Description: create node to the beginning of list
+ *
+ * Return: list_t
+ */
+list_t *create_node(list_t *tmp, const char *str)
+{
+	char *s;
+	int len = 0;
+
+	if (str != NULL)
+	{
+		s = strdup(str);
+		while (s[len] != '\0')
+			len++;
+	}
+
+	tmp->str = s;
+	tmp->len = len;
+
+	return (tmp);
+}
+
+/**
+ * add_node - add node
+ * @head: pointer to struct list_s pointer
+ * @str: string
+ *
+ * Description: add node to the beginning of list
+ *
+ * Return: list_t
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *tmp;
+
+	tmp = malloc(sizeof(list_t));
+
+	if (tmp == NULL)
+		return (NULL);
+	tmp = create_node(tmp, str);
+/*printf("%s %d\n", tmp->str, tmp->len);*/
+	if (*head != NULL)
+		tmp->next = *head;
+	*head = tmp;
+
+	return (tmp);
+}
