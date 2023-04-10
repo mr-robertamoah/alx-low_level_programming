cefb4c8a95494b8399f111b0eafaa43577f4c6df
Robert Amoah <mr_robertamoah@yahoo.com>
2023-04-11 00:44:58 +0100

    made more corrections to 2-append_text_to_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,31 +13,21 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, len = 0;
 	ssize_t check = 0;
-	char *buf;
 
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content == NULL)
-	{
-		buf = "";
-	}
-	else
+	if (text_content != NULL)
 	{
 		while (text_content[len] != '\0')
 			len++;
-
-		buf = malloc(sizeof(char) * len);
-		if (buf == NULL)
-			return (-1);
 	}
 
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
-	check = write(fd, buf, len);
-	free(buf);
+	check = write(fd, text_content, len);
 	close(fd);
 	if (check == -1 || check != len)
 		return (-1);
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,29 +13,24 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, len = 0;
 	ssize_t check = 0;
-	char *buf = NULL;
 
 	if (filename == NULL)
 		return (-1);
 
 	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
 
 	if (text_content != NULL)
 	{
 		while (text_content[len] != '\0')
 			len++;
-
-		buf = malloc(sizeof(char) * (len + 1));
-		if (buf == NULL)
-			return (-1);
-		buf = text_content;
 	}
 
-	check = write(fd, buf, len);
-	if (check == -1 || fd == -1)
+	check = write(fd, text_content, len);
+	if (check == -1)
 		return (-1);
 
 	close(fd);
-	free(buf);
 	return (1);
 }
