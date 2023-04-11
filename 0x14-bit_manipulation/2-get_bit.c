f3dd9758c6c43a9d0cf9cd4306d7bcc51be28943
Robert Amoah <mr_robertamoah@yahoo.com>
2023-04-11 09:12:50 +0100

    done with 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,77 +1,6 @@
 #include "main.h"
 
 /**
- * _pow - power
- * @i: int
- *
- * Description: find power of 2
- *
- * Return: unsigned int
- */
-unsigned int _pow(int i)
-{
-	unsigned int res = 1;
-
-	if (i == 0)
-		return (1);
-
-	while (i != 0)
-	{
-		res *= 2;
-		i--;
-	}
-
-	return (res);
-}
-
-/**
- * convert - convert
- * @b: binary as string
- * @res: pointer to unsigned int
- * @len: unsigned int
- *
- * Description: convert to decimal
- *
- * Return: void
- */
-void convert(
-	unsigned int *res, const char *b, unsigned int len
-)
-{
-	int i = 0, a = 0;
-
-	while (b[i] != '\0')
-	{
-		a++;
-		*res = *res + ((b[i] - '0') * _pow(len - a));
-		i++;
-	}
-}
-
-/**
- * has_non_binary - check
- * @b: binary as string
- *
- * Description: check if string is valid not binary
- *
- * Return: int
- */
-int has_non_binary(const char *b)
-{
-	int i = 0, status = 1;
-
-	while (b[i] != '\0')
-	{
-		if (b[i] != '1' && b[i] != '0')
-			return (status);
-		i++;
-	}
-
-	status--;
-	return (status);
-}
-
-/**
  * get_bit - get
  * @n: unsigned long int
  * @index: unsigned int
@@ -83,14 +12,16 @@ int has_non_binary(const char *b)
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int res = 0, len = 0;
+	while (n > 1)
+	{
+		if (index == (unsigned int) 0)
+			break;
+		n = n >> 1;
+		index--;
+	}
 
-	if (b == NULL || has_non_binary(b))
-		return (res);
+	if (index != (unsigned int) 0)
+		return (-1);
 
-	while (b[len] != '\0')
-		len++;
-	convert(&res, b, len);
-
-	return (res);
+	return (n & 1 ? 1 : 0);
 }
