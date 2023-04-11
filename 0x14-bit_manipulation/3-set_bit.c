7c277bcf1ebc16b224423aa2970bc4157e7fbc07
Robert Amoah <mr_robertamoah@yahoo.com>
2023-04-11 13:14:24 +0100

    corrected 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,6 +1,27 @@
 #include "main.h"
 
 /**
+ * count_bits - count
+ * @n: unsigned long int
+ *
+ * Description: count the number of binaries in int
+ *
+ * Return: unsigned int
+ */
+unsigned int count_bits(unsigned long int n)
+{
+	unsigned int count = 1;
+
+	while (n > 1)
+	{
+		n = n >> 1;
+		count++;
+	}
+
+	return (count);
+}
+
+/**
  * set_bit - set
  * @n: unsigned long int
  * @index: unsigned int
@@ -13,18 +34,22 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int i = 1;
+	unsigned int count = 0, count_i = 1;
 
 	if (index > 63)
 		return (-1);
-	while (i != *n)
+	count = count_bits(*n);
+	while (count_i != count)
 	{
 		if (index == 0)
 			break;
 		i = i << 1;
 		index--;
+		count_i = count_bits(i);
 	}
 
-	*n = *n | i;
+	if (index == 0)
+		*n = *n | i;
 
 	return (1);
 }
