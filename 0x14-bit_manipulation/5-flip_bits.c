a2f0e09b897fe81e7050e0e6b7022e7b4cabe944
Robert Amoah <mr_robertamoah@yahoo.com>
2023-04-11 11:20:38 +0100

    done with 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,78 +1,56 @@
 #include "main.h"
 
 /**
- * _pow - power
- * @i: int
+ * set_count - set
+ * @count: pointer unsigned int
+ * @n: pointer unsigned long int
+ * @m: pointer unsigned long int
  *
- * Description: find power of 2
- *
- * Return: unsigned int
- */
-unsigned int _pow(int i)
-{
-	unsigned int res = 1;
-
-	if (i == 0)
-		return (1);
-
-	while (i != 0)
-	{
-		res *= 2;
-		i--;
-	}
-
-	return (res);
-}
-
-/**
- * convert - convert
- * @b: binary as string
- * @res: pointer to unsigned int
- * @len: unsigned int
- *
- * Description: convert to decimal
- *
- * Return: void
- */
-void convert(
-	unsigned int *res, const char *b, unsigned int len
-)
-{
-	int i = 0, a = 0;
-
-	while (b[i] != '\0')
-	{
-		a++;
-		*res = *res + ((b[i] - '0') * _pow(len - a));
-		i++;
-	}
-}
-
-/**
- * has_non_binary - check
- * @b: binary as string
- *
- * Description: check if string is valid not binary
+ * Description: get the bit on an unsigned long int at a
+ * specific index
  *
  * Return: int
  */
-int has_non_binary(const char *b)
+void set_count(
+	unsigned int *count, unsigned long int *n,
+	unsigned long int *m
+)
 {
-	int i = 0, status = 1;
+	if (get_bit(*m, 0) != get_bit(*n, 0))
+		*count = *count + 1;
 
-	while (b[i] != '\0')
-	{
-		if (b[i] != '1' && b[i] != '0')
-			return (status);
-		i++;
-	}
-
-	status--;
-	return (status);
+	*m = *m >> 1;
+	*n = *n >> 1;
 }
 
 /**
  * get_bit - get
+ * @n: unsigned long int
+ * @index: unsigned int
+ *
+ * Description: get the bit on an unsigned long int at a
+ * specific index
+ *
+ * Return: int
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	while (n > 1)
+	{
+		if (index == (unsigned int) 0)
+			break;
+		n = n >> 1;
+		index--;
+	}
+
+	if (index != (unsigned int) 0)
+		return (-1);
+
+	return (n & 1 ? 1 : 0);
+}
+
+/**
+ * flip_bits - flip
  * @n: unsigned long int
  * @m: unsigned long int
  *
@@ -83,14 +61,18 @@ int has_non_binary(const char *b)
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int res = 0, len = 0;
+	unsigned long int i;
+	unsigned int count = 0;
 
-	if (b == NULL || has_non_binary(b))
-		return (res);
+	i = n >= m ? n : m;
 
-	while (b[len] != '\0')
-		len++;
-	convert(&res, b, len);
+	while (i > 1)
+	{
+		set_count(&count, &m, &n);
+		i = i >> 1;
+	}
 
-	return (res);
+	set_count(&count, &m, &n);
+
+	return (count);
 }
