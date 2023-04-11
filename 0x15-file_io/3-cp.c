9a3a3010b1b9cd125889a5516f71d2f83a6cdf8c
Robert Amoah <mr_robertamoah@yahoo.com>
2023-04-11 03:36:36 +0100

    made corrections to 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -24,19 +24,17 @@ int main(int ac, char *av[])
 	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 0664);
 	while ((check_from = read(fd_from, buf, 1024)))
 	{
+		if (fd_from == -1 || check_from == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+			exit(98);
+		}
 		check_to = write(fd_to, buf, check_from);
-		if (check_from == -1 || check_to == -1)
-			break;
-	}
-	if (fd_from == -1 || check_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
-	if (fd_to == -1 || check_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99);
+		if (fd_to == -1 || check_to == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			exit(99);
+		}
 	}
 	check_from = close(fd_from);
 	check_to = close(fd_to);
