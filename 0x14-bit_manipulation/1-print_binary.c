97e358cdc641074d1c979c81f6d3597e8eff827d
Robert Amoah <mr_robertamoah@yahoo.com>
2023-04-11 07:39:35 +0100

    done with 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,77 +1,6 @@
 #include "main.h"
 
 /**
- * _pow - power
- * @i: int
- *
- * Description: find power of 2
- *
- * Return: unsigned int
- */
-unsigned int _pow(int i)
-{
-	unsigned int res = 1;
-
-	if (i == 0)
-		return (1);
-
-	while (i != 0)
-	{
-		res *= 2;
-		i--;
-	}
-
-	return (res);
-}
-
-/**
- * convert - convert
- * @b: binary as string
- * @res: pointer to unsigned int
- * @len: unsigned int
- *
- * Description: convert to decimal
- *
- * Return: void
- */
-void convert(
-	unsigned int *res, const char *b, unsigned int len
-)
-{
-	int i = 0, a = 0;
-
-	while (b[i] != '\0')
-	{
-		a++;
-		*res = *res + ((b[i] - '0') * _pow(len - a));
-		i++;
-	}
-}
-
-/**
- * has_non_binary - check
- * @b: binary as string
- *
- * Description: check if string is valid not binary
- *
- * Return: int
- */
-int has_non_binary(const char *b)
-{
-	int i = 0, status = 1;
-
-	while (b[i] != '\0')
-	{
-		if (b[i] != '1' && b[i] != '0')
-			return (status);
-		i++;
-	}
-
-	status--;
-	return (status);
-}
-
-/**
  * print_binary - print
  * @n: binary to print
  *
@@ -81,14 +10,9 @@ int has_non_binary(const char *b)
  */
 void print_binary(unsigned long int n)
 {
-	unsigned int res = 0, len = 0;
-
-	if (b == NULL || has_non_binary(b))
-		return (res);
-
-	while (b[len] != '\0')
-		len++;
-	convert(&res, b, len);
-
-	return (res);
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+	_putchar(n & 1 ? '1' : '0');
 }
