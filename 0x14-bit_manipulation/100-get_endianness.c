f3fac28d9fc898b24d6f751962160ebb7799451a
Robert Amoah <mr_robertamoah@yahoo.com>
2023-04-12 04:36:50 +0100

    done with 100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * get_endianness - endianness status
+ *
+ * Description: determine whether or not a
+ * machine has little or big endianness
+ *
+ * Return: 0 if big endian and 1 if little
+ */
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *check = (char*)&i;
+
+	if (*check)
+		return (1);
+	else
+		return (0);
+}
