93a1bd515f19ea88ca8ca6d80e65f16907908712
Robert Amoah <mr_robertamoah@yahoo.com>
2023-05-18 17:06:45 +0100

    made corrections to 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,12 +1,18 @@
 #include "lists.h"
 
+/**
+* print_dlistint - print the n members of the dlistint
+* @h: node of a dlistint link
+* Return: number of nodes in the list
+*/
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t length = 0;
+
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		length += h->n;
+		length++;
 		h = h->next;
 	}
 
