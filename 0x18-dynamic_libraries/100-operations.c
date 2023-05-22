4ba3737cc17e774564dea01613bdf379678f3d57
Robert Amoah <mr_robertamoah@yahoo.com>
2023-05-22 11:00:19 +0100

    done with creating a dynamic library from a C file that contains basic maths operations

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,55 @@
+
+/**
+* add - add two integers
+* @a: integer
+* @b: integer
+* Return: integer
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* sub - subtract two integers
+* @a: integer
+* @b: integer
+* Return: integer
+*/
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* mul - product two integers
+* @a: integer
+* @b: integer
+* Return: integer
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* div - quotient two integers
+* @a: integer
+* @b: integer
+* Return: integer
+*/
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+* mod - remainder of two integers
+* @a: integer
+* @b: integer
+* Return: integer
+*/
+int mod(int a, int b)
+{
+	return (a % b);
+}
