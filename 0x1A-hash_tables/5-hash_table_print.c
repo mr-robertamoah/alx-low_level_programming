9fe01c2d5ef2f09e32490c35d5ed6adcf51a7438
Robert Amoah <mr_robertamoah@yahoo.com>
2023-06-10 00:37:02 +0100

    changed the order of printing the list in 5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -13,6 +13,20 @@ void print_node(hash_node_t *node, int printed)
 }
 
 /**
+* print_list - print linked list
+* @node: hash_node_t pointer
+* @printed: integer (0 or 1)
+*/
+void print_list(hash_node_t *node, int *printed)
+{
+	if (node->next != NULL)
+		print_list(node->next, printed);
+
+	print_node(node, *printed);
+	*printed = 1;
+}
+
+/**
 * hash_table_print - print hash table
 * @ht: hash table
 *
@@ -34,11 +48,15 @@ void hash_table_print(const hash_table_t *ht)
 		for (i = 0; i < ht->size; i++)
 		{
 			head = ht->array[i];
-			while (head)
+			if (head && head->next == NULL)
 			{
 				print_node(head, printed);
 				printed = 1;
 				head  = head->next;
+			}
+			else if (head && head->next)
+			{
+				print_list(head, &printed);
 			}
 		}
 	}
