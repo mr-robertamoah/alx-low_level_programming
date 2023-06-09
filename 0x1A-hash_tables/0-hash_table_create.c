edb6f4bdb24bbf77ec0c495fb024126ca36357b3
Robert Amoah <mr_robertamoah@yahoo.com>
2023-06-09 20:02:08 +0100

    corrected hash_tables.h to be betty compliant

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,13 +15,16 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *hash_table = malloc(sizeof(hash_table_t));
 	unsigned long int i = 0;
 
-	if (hash_table == NULL)
+	if (size == 0 || hash_table == NULL)
 		return (NULL);
 
 	hash_table->size = size;
 	hash_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (hash_table->array == NULL)
+	{
+		free(hash_table);
 		return (NULL);
+	}
 
 	for (; i < size; i++)
 		hash_table->array[i] = NULL;
